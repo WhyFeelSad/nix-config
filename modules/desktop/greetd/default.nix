03bce62bc93cfdecac31016b1e1813a59a573b2a
Aaron <the.sad@niceboy.org>
2025-01-01 16:10:49 +0800

    fix(modules/desktop): fix autologin

--- modules/desktop/greetd/default.nix
+++ modules/desktop/greetd/default.nix
@@ -11,7 +11,7 @@
       vt = 2;
       settings = rec {
         initial_session = {
-          command = "${lib.getExe pkgs.greetd.tuigreet} --remember --time --cmd ${lib.getExe' pkgs.niri "niri-session"}";
+          command = "${pkgs.niri}/bin/niri-session";
           user = "${myvars.userName}";
         };
         default_session = initial_session;

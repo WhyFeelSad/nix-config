04e63c92fc26b6d82f25a5748805abca0720d555
Aaron <the.sad@niceboy.org>
2025-01-01 12:27:14 +0800

    feat(modules/desktop): add greetd

---
+++ modules/desktop/greetd/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  pkgs,
+  myvars,
+  ...
+}:
+{
+  services.greetd = {
+    enable = true;
+    vt = 2;
+    settings = rec {
+      initial_session = {
+        command = "${lib.getExe pkgs.greetd.tuigreet} --remember --time --cmd ${lib.getExe' pkgs.niri "niri-session"}";
+        user = "${myvars.userName}";
+      };
+      default_session = initial_session;
+    };
+  };
+}

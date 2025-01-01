1b287c06b20e801cf6f2a624902441f08f3e8688
Aaron <the.sad@niceboy.org>
2025-01-01 20:44:02 +0800

    feat(home/programs): update swaylock config

--- home/programs/swaylock/default.nix
+++ home/programs/swaylock/default.nix
@@ -2,18 +2,23 @@
   programs.swaylock = {
     enable = true;
     settings = {
-      clock = true;
-      show-failed-attempts = true;
-      indicator = true;
-      indicator-radius = 200;
-      indicator-thickness = 20;
-      line-uses-ring = false;
-      grace = 0;
-      grace-no-mouse = true;
-      grace-no-touch = true;
-      datestr = "%d.%m";
-      fade-in = "0.1";
-      ignore-empty-password = true;
+      font = "Maple Mono NF CN";
+      font-size = 50;
+      indicator-radius = 100;
+      indicator-thickness = 10;
+      inside-color = "ffffff00";
+      key-hl-color = "5e81ac";
+      ring-color = "2e3440";
+      line-uses-ring = true;
+      separator-color = "e5e9f022";
+      text-color = "d8dee9ff";
+      layout-text-color = "d8dee9ff";
+      text-clear-color = "d8dee9ff";
+      text-caps-lock-color = "d8dee9ff";
+      indicator-idle-visible = true;
+      daemonize = true;
+      image = "/home/aaron/Pictures/wallpaper/akshar-dave-BcvPlibJyo0-unsplash.jpg";
+      scaling = "fill";
     };
   };
 }
